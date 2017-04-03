e6bac4a40c322b126a02b9459b2345d540df3a33
康桐 <kangtong1993@outlook.com>
2017-04-03 22:48:31 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/kangtong/nalingweather/model/ChinaCity.java
@@ -0,0 +1,16 @@
+package com.kangtong.nalingweather.model;
+
+public class ChinaCity {
+  public String id;
+  public String cityEn;
+  public String cityZh;
+  public String countryCode;
+  public String countryEn;
+  public String countryZh;
+  public String provinceEn;
+  public String provinceZh;
+  public String leaderEn;
+  public String leaderZh;
+  public String lat;
+  public String lon;
+}
