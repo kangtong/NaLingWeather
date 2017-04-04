cdce679dedeacafc4db1cccac72a4d93edcf682d
康桐 <kangtong1993@outlook.com>
2017-04-04 14:39:34 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/kangtong/nalingweather/MainActivity.java
+++ app/src/main/java/com/kangtong/nalingweather/MainActivity.java
@@ -1,7 +1,8 @@
 package com.kangtong.nalingweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
   }
 }
